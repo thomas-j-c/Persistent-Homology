c24e843e4602194e0fbc46567471547dd18eba45
thomas-j-c <everythingelsetomc@gmail.com>
2026-08-24 20:02:46 +0100

    play around

--- main.py
+++ main.py
@@ -10,13 +10,37 @@ structure = parser.get_structure("MyFirstStructure", "data/9ZBV.cif")
 # print(structure.center_of_mass())
 
 atoms = structure.get_atoms()
-
 coordinates = []
 
 for atom in atoms:
-    print(atom.coord)
-#split coordinates into x,y, z lists then you can use them in matplotlib.
-# add the project to git
+    coordinates.append(atom.get_coord().tolist())
+    
 
-plt.scatter()
+
+#Vietoris-Rips filtration
+filtration = gd.RipsComplex(points=coordinates, max_edge_length=3)
+tree = filtration.create_simplex_tree()
+tree.compute_persistence()
+pairs = tree.persistence_pairs()
+
+#intervals = tree.persistence_intervals_in_dimension(3)
+#print(intervals)
+
+
+fig = plt.figure()
+ax = fig.add_subplot()
+
+births = []
+deaths = []
+
+for pair in pairs:
+    try:
+        births.append(pair[1][0])
+        deaths.append(pair[1][1])
+    except IndexError:
+        continue
+
+ax.scatter(births, deaths, s=1)
+
+plt.show()
 
